3e77197c1068483ef2538fcf383ae4f5a926317d
Jack Mazzella <jackmazz10@gmail.com>
2025-12-18 16:21:48 -0500

    changed dmo prefix to dma

--- src/tests/demo.cpp
+++ src/tests/demo.cpp
@@ -5,57 +5,39 @@
 #include <iostream>
 
 int main() {
-    dm::Stage* stage = dm::Stage::load("../assets/stages/demo-stage-1.dmo");
+    dm::Stage* stage = dm::Stage::load("../assets/stages/demo-stage-1.dma");
     if (stage == nullptr) {
         std::cout << "Failed to load stage\n";
         return 1;
     }
     
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
     
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
     
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
     
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
     
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    dm::Actor::load("../assets/actors/demo-actor-2.dmo");
-    
-    /*
-    dm::Actor* actor = stage->findOccupant(1);
-    if (actor == nullptr) {
-        std::cout << "Failed to find actor\n";
-        return 1;
-    }
-    
-    std::cout << "stage-address: " << stage << "\n";
-    if (actor->isPlaced()) {
-        std::cout << "actor-parent-address: " << actor->placement()->parent() << "\n";
-    } else {
-        std::cout << "actor-parent-address: " << nullptr << "\n";
-    }
-    
-    std::cout << stage->toString() << std::endl;
-    dm::Actor::unload(actor);
-    */
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
+    dm::Actor::load("../assets/actors/demo-actor-2.dma");
     
     std::cout << stage->toString() << std::endl;
     dm::Stage::unload(stage);
